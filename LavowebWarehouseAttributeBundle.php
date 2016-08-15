0b386a539a71fb593a4f1c77715cc2002d292fe7
Aurélien Lavorel <aurelien@lavoweb.net>
2016-08-15 22:07:39 +0200

    [add] Initial commit

---
+++ LavowebWarehouseAttributeBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Lavoweb\Bundle\WarehouseAttributeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+/**
+ * Lavoweb Warehouse Attribute Bundle
+ *
+ * @author    Aurélien Lavorel <aurelien@lavoweb.net>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class LavowebWarehouseAttributeBundle extends Bundle
+{
+
+}
